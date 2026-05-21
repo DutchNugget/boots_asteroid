306a96531ab23ac39a6418d489fdcc5233683922
DutchNugget <232434153+DutchNugget@users.noreply.github.com>
2026-05-21 19:19:46 +0200

    added split method to asteroids

--- asteroids.py
+++ asteroids.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+import random
+from logger import log_event
 
 class Asteroid (CircleShape):
     def __init__(self, x, y, radius):
@@ -11,4 +13,25 @@ class Asteroid (CircleShape):
     
     def update (self, delta_time):
         self.position += self.velocity * delta_time
+
+    def split (self):
+        pygame.sprite.Sprite.kill(self)
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+
+            new_vector = random.uniform(20, 50)
+            Asteroid_oneV = pygame.math.Vector2.rotate(self.velocity, new_vector)
+            Asteroid_twoV = pygame.math.Vector2.rotate(self.velocity, -new_vector)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            newAS1 = Asteroid(self.position.x, self.position.y, new_radius)
+            newAS1.velocity = Asteroid_oneV * 1.2
+            newAS2 = Asteroid(self.position.x, self.position.y, new_radius)
+            newAS2.velocity = Asteroid_twoV * 1.2
+
+
+
+
     
--- main.py
+++ main.py
@@ -54,7 +54,14 @@ def main():
                 print("Game over!")
                 sys.exit()
             pass
-
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    pygame.sprite.Sprite.kill(shot)
+                    asteroid.split()
+        
         pygame.display.flip()
         delta_time = clock.tick(60) / 1000.0
         
